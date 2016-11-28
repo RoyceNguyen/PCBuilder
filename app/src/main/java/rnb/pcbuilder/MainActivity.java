f250d97362ed247e6a212646e74199bdf5416d4f
Royce Nguyen <hoangtu1997@gmail.com>
2016-11-27 22:27:10 -0500

    Added animations to the transitioning, added contactfragment transaction to mainactivity

--- app/src/main/java/rnb/pcbuilder/MainActivity.java
+++ app/src/main/java/rnb/pcbuilder/MainActivity.java
@@ -3,6 +3,7 @@ package rnb.pcbuilder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -80,8 +81,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_contact) {
+            FragmentTransaction tran = fm.beginTransaction();
+            tran.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            tran.replace(R.id.content_main, new ContactFragment());
+            tran.commit();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
